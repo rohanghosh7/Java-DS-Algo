ad3d4d1c12cca2514212394eb3b0199b396c5ec9
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-15 20:49:20 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+package dataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		byte x = 2;
+		System.out.println(x);
+		int age = 10;
+		System.out.println(age);
+		float rateOfInterest = 2.3f;
+		System.out.println(rateOfInterest);
+		double pi = 3.1428571;
+		System.out.println(pi);
+		boolean boo = true;
+		System.out.println(boo);
+		char ch = 'A';
+		System.out.println(ch);
+	}
+
+}
