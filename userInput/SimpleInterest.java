16d219ab933acceb8c763ae0292c5c7b344320ba
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-18 19:40:13 +0530

    Add files via upload

---
+++ userInput/SimpleInterest.java
@@ -0,0 +1,15 @@
+package userInput;
+import java.util.Scanner;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int principal = sc.nextInt();
+		float rate = sc.nextFloat();
+		int time = sc.nextInt();
+		float simpleInterest = principal * rate * time / 100;
+		System.out.println("The simple interest is " + simpleInterest);
+	}
+
+}
