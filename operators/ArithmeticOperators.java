19ec28be50e5c30982ca06698478bdc4e84062bb
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-21 02:18:21 +0530

    Add files via upload

---
+++ operators/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+package operators;
+
+public class ArithmeticOperators {
+	
+	public static void main(String[] args) {
+		
+		int a = 23;
+		int b = 40;
+		int c = a+b;
+		int d = a-b;
+		int e = a*b;
+		int f = b/a;
+		int g = a%b;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+		
+	}
+}
