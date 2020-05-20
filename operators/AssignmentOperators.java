1e6575cf31575c857a69924a2d5707a8621e7d27
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-21 02:18:43 +0530

    Add files via upload

---
+++ operators/AssignmentOperators.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class AssignmentOperators {
+
+	public static void main(String[] args) {
+		int a = 20;
+		a+= 2;
+		System.out.println(a);
+		//Similarly we can do the same for -,*,/ and others
+	}
+
+}
