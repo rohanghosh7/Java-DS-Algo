a78f1ba781002c71735da276fd4d77f8881a2653
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-22 02:36:08 +0530

    Add files via upload

---
+++ operators/ComparisonOperators.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class ComparisonOperators {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 7;
+		System.out.println(a > b);
+		System.out.println(a < b);
+		System.out.println(a == b);
+		System.out.println(a != b);
+		System.out.println(a >= b);
+		System.out.println(a <= b);
+	}
+
+}
