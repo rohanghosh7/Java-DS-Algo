934d9266007e60c393f35c6071e17a5047b1e7b2
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-22 02:36:29 +0530

    Add files via upload

---
+++ operators/LogicalOperators.java
@@ -0,0 +1,28 @@
+package operators;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		//Logical AND
+		int number = 45;
+		if(number >= 1 && number <= 100)
+			System.out.println("Number is in range");
+		else
+			System.out.println("Number is out of range");
+		
+		//Logical OR
+		int grade = 12;
+		if(grade == 10 || grade == 12)
+			System.out.println("You can give board exams");
+		else
+			System.out.println("You can not give board exams");
+		
+		//Logical NOT
+		grade = 11;
+		if(!(grade == 10 || grade == 12))
+			System.out.println("You can not give board exams");
+		else
+			System.out.println("You can  give board exams");
+	}
+
+}
