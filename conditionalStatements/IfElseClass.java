9c54b1693281da9c62620c8bdc757fedaf026b9e
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-23 02:19:19 +0530

    Add files via upload

---
+++ conditionalStatements/IfElseClass.java
@@ -0,0 +1,14 @@
+package conditionalStatements;
+
+public class IfElseClass {
+
+	public static void main(String[] args) {
+		int noOfPetals = 10;
+		if(noOfPetals % 2 == 0)
+			System.out.println("He loves me not");
+		else
+			System.out.println("He loves me");
+
+	}
+
+}
