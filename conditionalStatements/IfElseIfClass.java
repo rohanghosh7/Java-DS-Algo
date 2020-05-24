41316f84a289d0f8d4b0e20e2d76859d43bedce2
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-24 11:32:14 +0530

    if else if

---
+++ conditionalStatements/IfElseIfClass.java
@@ -0,0 +1,18 @@
+package conditionalStatements;
+
+public class IfElseIfClass {
+
+	public static void main(String[] args) {
+		int number = 35;
+		if(number <= 10)
+			System.out.println("Number is less than 10");
+		else if(number > 10 && number <= 20)
+			System.out.println("Number is greater than 10 and less than 20");
+		else if(number > 20 && number <= 30)
+			System.out.println("Number is greater than 20 and less than 30");
+		else
+			System.out.println("Number is greater than 30");
+
+	}
+
+}
