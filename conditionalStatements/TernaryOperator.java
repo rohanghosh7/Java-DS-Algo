ee756f26cc4567766fe8bfb1d4f53e9ddbb5e3aa
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-26 01:50:46 +0530

    Add files via upload

---
+++ conditionalStatements/TernaryOperator.java
@@ -0,0 +1,14 @@
+package conditionalStatements;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		int a = 17;
+		int b = 10;
+		int maxOfBoth;
+		maxOfBoth = (a > b)? a : b;
+		System.out.println("Maximum number is " + maxOfBoth);
+
+	}
+
+}
