3c8a0ed1aa277ab582cb42d5014bfc8cb5d7f29b
Rohan Ghosh <iamronnie40@gmail.com>
2020-07-01 09:09:56 +0530

    optimised prime

---
+++ loops/OptimisedPrime.java
@@ -0,0 +1,36 @@
+package loops;
+
+import java.util.Scanner;
+
+public class OptimisedPrime {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number");
+		int n = sc.nextInt();
+		
+		int i;
+		
+		boolean isPrime = true;
+		
+		for(i = 2;i * i <= n; i++)
+		{
+			if(n % i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		if(n == 1)
+			isPrime = false;
+		
+		if(isPrime)
+			System.out.println(n + " is prime");
+		else
+			System.out.println(n + " is not prime");
+
+	}
+
+}
